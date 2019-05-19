2ca05aac58d06f7e981c8d3a84300ad084caf57b
v.nostyte <v.nostyte@gmail.com>
2019-05-19 22:29:09 +0300

    Commit

--- loadImage.h
+++ loadImage.h
@@ -5,6 +5,7 @@
 class Image_texture
 {
 public:
+	//
 	Image_texture () {}
 	void load(const char* photoName, SDL_Renderer* ren);
 	static SDL_Texture* load_texture(const char* photoName, SDL_Renderer* ren);
